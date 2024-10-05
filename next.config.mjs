4de7ef60e6e24b7e96ef0716993d6fdb94ba6910
wenjebs <wenjieb2@gmail.com>
2024-10-05 18:54:08 +0800

    Fix image path

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  images: {
+    loader: "akamai",
+    path: "https://wenjebs.github.io/personal-website/",
+  },
 };
 
 export default nextConfig;
